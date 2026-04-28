4c50baf6d5508845fe41dd7bb4a28076b7c37da0
raulshma <raulshma@gmail.com>
2026-04-28 12:43:14 +0530

    feat: Enhance APK output naming and enable ABI splits for optimized builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
